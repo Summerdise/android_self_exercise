4d5746c210c20ce52da3fddf356c9099510298da
summerdise <350464517@qq.com>
2020-10-30 18:04:49 +0800

    [zmc] feat: add two back main activity function

--- app/src/main/java/com/example/helloworld/FrameLayout.java
+++ app/src/main/java/com/example/helloworld/FrameLayout.java
@@ -3,12 +3,33 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FrameLayout extends AppCompatActivity {
+
+    TextView textView;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_layout);
+
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FrameLayout.this.finish();
+            }
+        });
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FrameLayout.this.finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -9,14 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.btn_1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        button2 = findViewById(R.id.btn_2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FrameLayout.class);
