925c920ad72228d3bfd5a72123a13443b6981cbe
summerdise <350464517@qq.com>
2020-11-11 10:06:14 +0800

    [zmc] feat: add recyclerview layout

---
+++ app/src/main/java/com/example/helloworld/RecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+    }
+}
